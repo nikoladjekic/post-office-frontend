836d2e9ea8612dbc9d2af7a6eaa525ecd964fddf
nikoladjekic <nikoladjekic.dev@gmail.com>
2023-11-11 20:15:24 +0100

    add client module and db url

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -23,6 +24,7 @@ import { PostOfficesComponent } from './home-page/post-offices/post-offices.comp
     FormsModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    HttpClientModule,
     MatButtonModule,
     MatDividerModule
   ],
