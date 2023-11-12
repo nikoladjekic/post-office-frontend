d064e4a5bb8f076db8aa4f75fbefe475b7fab979
nikoladjekic <nikoladjekic.dev@gmail.com>
2023-11-12 14:23:39 +0100

    fetch shipments in the component and display them in a mat table

--- src/app/home-page/shipments/shipments.component.ts
+++ src/app/home-page/shipments/shipments.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Shipment } from 'src/models/shipment.interface';
+import { ShipmentService } from 'src/services/shipment.service';
 
 @Component({
   selector: 'app-shipments',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shipments.component.scss']
 })
 export class ShipmentsComponent implements OnInit {
+  allShipments: Shipment[] = [];
+  tableColumns: string[] = ['type', 'origin', 'destination', 'delivered', 'weight', 'office'];
 
-  constructor() { }
+  constructor(private _shipments: ShipmentService) { }
 
   ngOnInit() {
+    this.getAllShipments();
+  }
+
+  getAllShipments(): void {
+    this._shipments.getAllShipments().subscribe(
+      response => {
+        this.allShipments = response;
+      },
+      err => {
+        console.error(err);
+      }
+    );
   }
 
 }
