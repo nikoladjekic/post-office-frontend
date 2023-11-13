d79e38b5fb61cefd3a2b050f365762de19d35e80
nikoladjekic <nikoladjekic.dev@gmail.com>
2023-11-13 12:11:16 +0100

    add pagination and filtering for shipments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,6 +33,10 @@ import { PostOfficesComponent } from './home-page/post-offices/post-offices.comp
     MatButtonModule,
     MatDividerModule,
     MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatInputModule,
+    MatFormFieldModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home-page/shipments/shipments.component.ts
+++ src/app/home-page/shipments/shipments.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
 import { Shipment } from 'src/models/shipment.interface';
 import { ShipmentService } from 'src/services/shipment.service';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-shipments',
@@ -8,8 +11,11 @@ import { ShipmentService } from 'src/services/shipment.service';
   styleUrls: ['./shipments.component.scss']
 })
 export class ShipmentsComponent implements OnInit {
-  allShipments: Shipment[] = [];
+  allShipments: MatTableDataSource<Shipment>;
   tableColumns: string[] = ['id', 'type', 'origin', 'destination', 'delivered', 'weight', 'office'];
+
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
   constructor(private _shipments: ShipmentService) { }
 
@@ -20,12 +26,18 @@ export class ShipmentsComponent implements OnInit {
   getAllShipments(): void {
     this._shipments.getAllShipments().subscribe(
       response => {
-        this.allShipments = response;
+        this.allShipments = new MatTableDataSource(response);
+        this.allShipments.paginator = this.paginator;
+        this.allShipments.sort = this.sort;
       },
       err => {
         console.error(err);
       }
     );
+  }
+
+  applyFilter(filterValue: string) {
+    this.allShipments.filter = filterValue.trim().toLowerCase();
   }
 
 }
